4ace0d12fe509251148526ff2758e1498420b5e1
shyamlal98 <shyaml.cse16@gmail.com>
2021-12-31 14:02:53 +0530

    updated with postman endpoins in a file

--- server.js
+++ server.js
@@ -9,14 +9,8 @@ const db = require("./app/models");
 db.sequelize.sync(); 
 global.__basedir = __dirname+"/app";
 global.__baseurl = "http://localhost:8080"
-// process.on('unhandledRejection', function(err) {
-//   console.log(err);
-// });
-
 const leadRoute = require('./app/routes/leads.route')
 app.use('/api/leads', leadRoute);
-
-
 app.get('/',(req,res)=>{
   res.send("Hello");
 })
